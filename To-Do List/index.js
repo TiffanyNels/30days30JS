d89675524c6c5fdeabec61da893b270fc42b1e68
Tiffany Nels <tnelsweb@gmail.com>
2024-02-03 14:23:15 +0200

    Third commit

--- To-Do List/index.js
+++ To-Do List/index.js
@@ -1,18 +1,27 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 const invalidEntry = document.querySelector(".invalid-entry");
-const addButton = document.querySelector(".add-button");
+
+document.addEventListener("DOMContentLoaded", function () {
+    showTask();
+});
 
 function addTask() {
     if (inputBox.value === '') {
         invalidEntry.style.display = "block";
     } else {
-        invalidEntry.style.display = "none"; 
+        invalidEntry.style.display = "none";
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        li.innerHTML = `${inputBox.value} <span class="remove-task" onclick="removeTask(this)">x</span>`;
         listContainer.appendChild(li);
+
+        li.addEventListener("click", function () {
+            li.classList.toggle("checked");
+            saveData();
+        });
     }
     inputBox.value = "";
+    saveData();
 }
 
 inputBox.addEventListener("keydown", function (event) {
@@ -20,3 +29,29 @@ inputBox.addEventListener("keydown", function (event) {
         addTask();
     }
 });
+
+function removeTask(element) {
+    let taskItem = element.parentElement;
+    taskItem.remove();
+    saveData();
+}
+
+function saveData() {
+    localStorage.setItem("todoList", listContainer.innerHTML);
+}
+
+function showTask() {
+    const savedData = localStorage.getItem("todoList");
+
+    if (savedData) {
+        listContainer.innerHTML = savedData;
+        document.querySelectorAll('li').forEach(item => {
+            item.addEventListener("click", function () {
+                item.classList.toggle("checked");
+                saveData();
+            });
+        });
+    }
+}
+
+showTask();
